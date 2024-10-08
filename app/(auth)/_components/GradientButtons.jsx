7e056a3f28b0446d754563a3b34faba9988f2ee4
Timothy <115435697+Ifiora-Timothy@users.noreply.github.com>
2024-10-09 00:39:00 +0100

    minor changes

--- app/(auth)/_components/GradientButtons.jsx
+++ app/(auth)/_components/GradientButtons.jsx
@@ -1,9 +1,13 @@
+// "use client";
 import { Button } from "@/components/ui/button";
 
-const GradientButtons = ({ Icon, text }) => {
+const GradientButtons = ({ text, children, ...props }) => {
   const gradientColors = ["#F5F5F5", "#C71585", "#8B008B", "#4B0082"];
   return (
-    <div className="w-full  h-[40px]  rounded-lg p-[0.8px]  bg-gradient-to-t from-slate-600 from-0% to-slate-400 to-100%   ">
+    <div
+      {...props}
+      className="w-full  h-[40px]  rounded-lg p-[0.8px]  bg-gradient-to-t from-slate-600 from-0% to-slate-400 to-100%   "
+    >
       <Button
         className={` w-full h-full
       relative bg-clip-content   overflow-hidden rounded-lg g-[#4B0082]  transition-all duration-300
@@ -14,8 +18,8 @@ const GradientButtons = ({ Icon, text }) => {
             "conic-gradient(from 270deg at 50% 50%, #4B0082 0deg, #C71585 48.25deg, #4B0082 260deg)",
         }}
       >
-        <div className="bg-">
-          <Icon />
+        <div className="" id="dds">
+          {children}
         </div>
         <span className="relative text-left bg-n-200 grow  px-4 py-3 font-medium text-white text-sm">
           {text}
