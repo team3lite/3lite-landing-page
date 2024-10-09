508eef21c761194449eef4b9da0144b793ae6029
charleschinedum <charleschinedum2@gmail.com>
2024-10-09 18:38:33 +0000

    Update app testimonials with new brand name

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -76,18 +76,18 @@ export const testimonials = [
     name: "Josh Sparks",
     title: "Seamless Transactions!",
     description:
-      "The wallet feature is a life-saver! I can send payments just as easily as sending a message—no complicated wallet addresses needed. Suift has made transactions simple and secure.",
+      "The wallet feature is a life-saver! I can send payments just as easily as sending a message—no complicated wallet addresses needed. 3lite has made transactions simple and secure.",
   },
   {
     name: "Big Cee",
     title: "Totally Amazing!",
     description:
-      "I’ve never felt more in control of my privacy. Suift's user-friendly design and blockchain-based security make it my go-to messaging app for all my personal and professional communications.",
+      "I’ve never felt more in control of my privacy. 3lite user-friendly design and blockchain-based security make it my go-to messaging app for all my personal and professional communications.",
   },
   {
     name: "Ugwu Chidi",
     title: "Unmatched Privacy and Speed",
     description:
-      "Suift has completely changed how our team communicates. With top-notch privacy features and lightning-fast messaging, we can stay connected securely without any worries about data leaks!",
+      "3lite has completely changed how our team communicates. With top-notch privacy features and lightning-fast messaging, we can stay connected securely without any worries about data leaks!",
   },
 ];
